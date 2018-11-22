98ffe68c44f229ec7767ccaa749e8b96f4e1ef4c
marynavoitenko <maryna.voitenko@gmail.com>
2018-11-21 21:40:41 -0500

    Initialize repository

---
+++ lib/mv_palindrome.rb
@@ -0,0 +1,6 @@
+require "mv_palindrome/version"
+
+module MvPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mv_palindrome/version.rb
@@ -0,0 +1,3 @@
+module MvPalindrome
+  VERSION = "0.1.0"
+end
